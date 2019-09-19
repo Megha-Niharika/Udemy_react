4197d327bb5676d996da9464739fea31bcdccf12
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-19 12:57:53 +0530

    change1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App  name="Abhay" age="28"/>, document.getElementById('root'));
 
-ReactDOM.render(<App  name="vivi" age="29"/>, document.getElementById('root1'));
+var app =(
+    <div>
+<App  name="Abhay" age="28"/>
+<App  name="vivi" age="29"/>
+</div>
+);
+ReactDOM.render(app, document.getElementById('app'));
+
+//ReactDOM.render(, document.getElementById('root1'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
